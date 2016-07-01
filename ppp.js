d7591d2a18c642a30a49da9d881bf92bb803a922
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-07-01 11:08:19 -0400

    JS file now includes and object constructor

--- ppp.js
+++ ppp.js
@@ -4,22 +4,34 @@ var inputStudent;
 
 
 
-//Bubble object constructor
-// function newBubble(keyword, text, student)
+Bubble object constructor
+function newBubble(nbKeyword, nbText, nbStudent, nbTime){
+    this.nbKeyword = keyword,
+    this.nbText = text,
+    this.nbStudent = student,
+    this.nbTime = time
+}
 
 
 //New Pain-Point Bubble
-function createBubble(keyword, text, student){
+function createBubbleDiv(keyword, text, student){
+    //Variables to create the new bubble Div
     var bubbleDiv = $("<div>").attr({class: "bubble"});
     var keyword = $("<h3>").text(keyword);
     var p = $("<p>").text(text);
     if(student){
-        var bMod = $("<b>");
-        var s = $("<p>").append(bMod).text(student);
+        // var bMod = $("<b>");
+        var s = $("<p><b>").text(student);
     }
+
+    //Append everything to the screen
     bubbleDiv.append(keyword);
     bubbleDiv.append(p);
-    $("#existingBubblesDiv").append(bubbleDiv)
+    bubbleDiv.append(s);
+    $("#existingBubblesDiv").append(bubbleDiv);
+
+    //Create a new object for the bubble
+    newBubble(keyword, text, student, /*moment.js time function?*/)
 }
 
 //Basic NLP API logic
@@ -28,9 +40,10 @@ function createBubble(keyword, text, student){
 function callback(response){
     console.log(response);
     var reply = response.keywords[0].text;
-    createBubble(reply, inputText, inputStudent);
+    createBubbleDiv(reply, inputText, inputStudent);
 }
 
+//Keyword analysis
 $(".btn").on("click", function(){
             inputText = $("textarea").val();
             inputStudent = $("#userName").val();
@@ -49,5 +62,7 @@ $(".btn").on("click", function(){
                 }
             }).done(callback);
 
-            $("form-control").val("URL");//Shouldn't this clear the form??
+            $(".form-control").val("");//Shouldn't this clear the form??
         })
+
+//
